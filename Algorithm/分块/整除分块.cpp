be4127800750e8775057236e570a9da632164b48
KKK <101380772+scannerkkk@users.noreply.github.com>
2022-10-21 11:52:11 +0800

    Create 整除分块.cpp

---
+++ Algorithm/分块/整除分块.cpp
@@ -0,0 +1,7 @@
+// ll res = 0,s = 0,n;
+//     cin >> n;
+//     for(ll i = 1;i <= n;i = s + 1) {
+//         s = n / (n / i);
+//         res += (s - i + 1) * (n / i);
+//     }
+//     cout << res << endl;
