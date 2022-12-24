dfd77d67898636b661e3dd8f0010ca64bce5e088
KKK <101380772+scannerkkk@users.noreply.github.com>
2022-12-24 16:11:51 +0800

    Update 快读快写.cpp

--- Algorithm/技巧/快读快写.cpp
+++ Algorithm/技巧/快读快写.cpp
@@ -1,28 +1,31 @@
-#include <bits/stdc++.h>
-using namespace std;
-char *p1,*p2,buf[100000];
-#define nc() (p1==p2 && (p2=(p1=buf)+fread(buf,1,100000,stdin),p1==p2)?EOF:*p1++)
+char *p1, *p2, buf[100000];
+#define nc() (p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 100000, stdin), p1 == p2) ? EOF : *p1++)
 inline int read()
 {
-    int x = 0,f = 1;
+    int x = 0, f = 1;
     char ch = nc();
-    while(ch < '0'||ch > '9')
+    while (ch < 48 || ch > 57)
     {
-        if(ch == '-') f = -1;
+        if (ch == '-')
+            f = -1;
         ch = nc();
     }
-    while(ch >= '0'&&ch<='9') x = x * 10 + ch - '0',ch = nc();
+    while (ch >= 48 && ch <= 57)
+        x = x * 10 + ch - 48, ch = nc();
     return x * f;
 }
-inline void write(int x){
-    if(x > 9) write(x / 10);
-    if(x < 0) putchar('-'),x = -x;
-    putchar(x % 10 + '0');
-}
-int main() {
-    int n;
-    while((n = read()),n){
-        write(n);
-        putchar(' ');
+inline void write(int x)
+{
+    if (x == 0)
+    {
+        putchar('0');
+        return;
     }
+    int len = 0, k1 = x, c[10005];
+    if (k1 < 0)
+        k1 = -k1, putchar('-');
+    while (k1)
+        c[len++] = k1 % 10 + '0', k1 /= 10;
+    while (len--)
+        putchar(c[len]);
 }
